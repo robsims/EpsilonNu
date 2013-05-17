a2de7a898a59a8e874f5bf6376cbb553a54d4af2
Rob SIms <rsims2013@gmail.com>
2013-05-16 23:43:59 -0400

    Beginning of PHP development

--- pages/header.php
+++ pages/header.php
@@ -1,1 +1,17 @@
-##Test
+<div id="header">
+    
+</div>
+<div id="nav">
+    <div id="navpadding">
+        <ul>
+            <li><b>Home</b></li>
+            <li><a href="brothers.html">Brothers</a></li>
+            <li><a href="history.html">History</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="links.html">Links</a></li>
+            <li><a href="alumni.html">Alumni</a></li>
+            <li><a href="nus.html">Newsletter</a></li>
+            <li><a href="gnp.html">GNP</a></li>
+        </ul>
+    </div>
+</div>
