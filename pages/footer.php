a47c43ef0f8370cd24f10c32453b65138a91fad9
rsims <rsims2013@gmail.com>
2013-05-18 00:33:44 -0400

    Fixed typo in Grinnell and Updated year.

--- pages/footer.php
+++ pages/footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
     <p class="footer_text">
-        Copyright &copy; 2012 | Epsilon Nu - Kappa Kappa Psi<br /> 
-        110 Grinnel Road, George N. Parks Minuteman Marching Band Building, University of Massachusetts, Amherst, MA 01003<br /> 
+        Copyright &copy; 2013 | Epsilon Nu - Kappa Kappa Psi<br /> 
+        110 Grinnell Road, George N. Parks Minuteman Marching Band Building, University of Massachusetts, Amherst, MA 01003<br /> 
     </p>
     </div>
 </div>
