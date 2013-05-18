1238f70d9443144feff994a7efff3c1a58214731
rsims <rsims2013@gmail.com>
2013-05-17 22:06:12 -0400

    Completed refactoring of index page.

--- index.php
+++ index.php
@@ -8,69 +8,53 @@
 
 
 <body>
-    <div id="page_container">
+    <div id="page_container"> 
+        
         <?php
             include 'pages/header.php';
         ?>
             
             <div id="content">
-                <div id="content_padding"><center>
+                <div id="content_padding">
                     <div style="text-align: left;">
                         <h2 style="margin: 0pt; padding: 0pt;">Latest News</h2>
                         <hr style="width: 100%; height: 2px;" />
-                        <p><strong>December 8, 2012:</strong></p>
-                        <p>Epsilon Nu would like to congratulate the newly initiated Brothers of the Sigma Alpha chapter of Phi Mu Alpha:<br>
-                        <br>
-                        Taylor Allen<br>
-                        Bobby Bergeron<br>
-                        Chris Garbacik<br>
-                        Bobby Gentile<br>
-                        Chris Godden<br>
-                        Jimi Hayes<br>
-                        Jonathan McHatton</p>
-                        <p>Here's to many years of making music together.</p>
-                
-                <p><span style="font-weight: bold;"><br /> 
-                December 1, 2012:</span><br />
-                </p>
-            </div>
-<div style="text-align: left;">
-<p><span style="font-weight: bold;"><br /> 
-    </span>Congratulations to the newly initiated Brothers of the
-    Beta Omega class!<br /> <br />
-    Owen Kerrigan<br />
-  Katie Wheeler<br>
-  David Orleck<br>
-  Matt Chastain<br>
-  Ryan Barry<br>
-  Joseph Gougen<br>
-  Ethan Miller  </p>
-  <p>In addition, we would like to extend our congratulations to the newly initiated Sisters of Delta Delta - Tau Beta Sigma:</p>
-  <p>Cece Barnes<br>
-    Stephanie Mullane<br>
-    Sarah Hackett<br>
-    Megan Joyce<br>
-    Morgan McGrane<br>
-    Shannon Mackey
-  </p>
-  <hr style="width: 100%; height: 2px;" />
-<br /> <span style="font-weight: bold;"></span></div>
+                        
+                            <?php
+                                include 'pages/latestnews.php';
+                            ?>
+                            
+                    </div>
+                    
+                    <center>        
+                        <h3 style="margin: 0pt; padding: 0pt;">Kappa Kappa Psi Creed</h3><br />
+                        
+                        
+                        We, the Brothers of Kappa Kappa Psi, believe that service to the college or<br />
+                        university band program fosters responsibility, loyalty, and leadership; that a<br />
+                        spirit of brotherhood is enhanced by the participation in a band program; that<br /> 
+                        music is a universal language and truly the greatest of the arts; and that through<br /> 
+                        fraternal participation, each member will strive for the highest.<br /> 
+                        
+                    </center> <br /> <br />
+                    
+                    <div style="text-align: center;">
+                        Click <a href="hymn.mp3" target="_blank">HERE</a> to hear the Brothers of Epsilion Nu singing our national hymn!
+                    </div>
+                    <br /> <br />
 
-<h3 style="margin: 0pt; padding: 0pt;">Kappa Kappa Psi
-Creed</h3>
-<br /> We, the Brothers of Kappa Kappa Psi, believe that service to the
-college or<br /> university band program fosters responsibility, loyalty, and
-leadership; that a<br /> spirit of brotherhood is enhanced by the participation in a band
-program; that<br /> music is a universal language and truly the greatest of the arts; and
-that through<br /> fraternal participation, each member will strive for the highest.<br /> </center> <br /> <br />
-<div style="text-align: center;">Click <a href="hymn.mp3" target="_blank">HERE</a> to hear the Brothers of Epsilion Nu singing our national hymn!<br /> <br /></div>
-<br />
-<div style="text-align: center;">Kappa Kappa Psi is a national honorary fraternity for college bandsmen
-founded in 1919. See the <a href="history.html">History</a> page for more information. <br /></div>
-<br /></div>
-</div>
-<?php
-    include 'pages/footer.php';
-?>
+                    <div style="text-align: center;">
+                        Kappa Kappa Psi is a national honorary fraternity for college bandsmen founded in 1919. See the <a href="history.html">History</a> page for more information. 
+                    </div>
+                    <br />
+                </div>
+            </div>
+            
+        <?php
+            include 'pages/footer.php';
+        ?>
+        
+    </div>
 </body>
+
 </html>
---
+++ pages/latestnews.php
@@ -0,0 +1,56 @@
+<!-- Use the following code for creating a new update
+ <p><strong>DATE</strong></p>
+ <p>INFO</p>
+ Use multiple paragraphs for info as necessary.
+-->
+
+<p><strong>May 17, 2013</strong></p>
+<p>Congratulations to Epsilon Nu Brother Joseph Goguen on your acceptance to the National Intercollegiate Band!  We're excited to see you at National Convention 2013!</p>
+ 
+<p><strong>May 4, 2013</strong></p>
+<p>Congratulations to the newly initiated Brothers of Sigma Alpha - Phi Mu Alpha:</p>
+<p> Alex Palango<br />
+    Andy Huynh<br />
+    Rick O'Leary<br />
+    Robert Fredette<br />
+    William Blazeski<br />
+    Brad Kronick<br />
+    Brandon Babbin</p>
+
+<p><strong>April 28, 2013</strong></p>
+<p>Congratulations to the newly initiated Sisters of Lambda Phi - Sigma Alpha Iota:</p>
+<p> Emily Gluck<br />
+    Gen Shephard<br />
+    Kimberly Meader<br />
+    Julie MacDonald<br />
+    MaryCatherine Lynch<br />
+    Laura Johnson<br />
+    Colleen Regan<br />
+    Liana Ascolese<br />
+    Emily Wright<br />
+    Emily Ethier<br />
+    Meghan Pace<br />
+    Sabina Gessay<br />
+    Pam McCaddin<br />
+    Kaz Au</p>
+
+<p><strong>April 15, 2013</strong></p>
+<p>Epsilon Nu welcomes their newly elected officers for the 2013-2014 school year!</p>
+<p> Adam Whitcomb - President<br />
+    Chris Moehringer - Vice President of Membership<br />
+    Bradley Sampson - Vice President of Service<br />
+    Katie Wheeler - Recording Secretary<br />
+    Michael Bates - Treasurer<br />
+    Vishu Nair - Alumni Relations Officer<br />
+    Ryan Barry - Corresponding Secretary<br />
+    Ethan Miller - Historian<br />
+    Parliamentarian - Andrew Lyczmanenko</p>
+
+<p><strong>April 11, 2013:</strong></p>
+<p>Epsilon Nu would like to congratulate the newly initiated Sisters of Delta Delta - Tau Beta Sigma</p>
+<p>Elizabeth Streitfeld<br />
+   Erica Davignon<br />
+   Hillary Bylicki</p>
+
+
+    
