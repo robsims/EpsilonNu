cc018e018fee61a10a992ef9ba2ff385d869806d
rsims <rsims2013@gmail.com>
2013-05-19 19:47:44 -0400

    Refactored brothers page, fixed links in header.

---
+++ brothers.php
@@ -0,0 +1,190 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <title>Epsilon Nu - Kappa Kappa Psi | University of
+Massachusetts</title>
+
+
+  <link href="styles.css" type="text/css" rel="stylesheet">
+
+</head>
+
+
+<body>
+    <div id="page_container">
+        <?php
+            include 'pages/header.php';
+        ?>
+        
+        <div id="content">
+            <div id="content_padding">
+                <h2 style="margin: 0pt; padding: 0pt;"><br /></h2>
+                <h2 style="margin: 0pt; padding: 0pt;">Brothers</h2>
+                <hr />
+
+                <table style="width: 607px; height: 266px;">
+                    <tbody>
+                        <tr>
+                            <td valign="top" width="390">
+                                <p>
+                                  Adam Whitcomb - <i>President</i><br /> 
+                                  Chris Moehringer - <i>Vice President of Membership</i><br /> 
+                                  Bradley Sampson - <i>Vice President of Service</i><br /> 
+                                  Katie Wheeler - <i>Recording Secretary</i><br /> 
+                                  Michael Bates - <i>Treasurer</i><br>
+                                  Ryan Barry - <i>Corresponding Secretary</i> <br /> 
+                                  Vishal Nair - <i>Alumni Relations Officer</i><br /> 
+                                  Ethan Miller - <i>Historian</i><br /> 
+                                  Andrew Lyczmanenko - <i>Parliamentarian</i><br /> 
+                                  Eddie Rouxinol<br />
+                                  Charles Urbanowski<br />
+                                  Richard Cabral<br />
+                                </p>
+                            </td>
+                            
+                            <td valign="top">                                
+                                Andrew deMelo<br />
+                                Owen Kerrigan<br />
+                                David Orleck<br />
+                                Matt Chastain<br />
+                                Joseph Gougen<br />
+                                Bolutare Doubeni<br />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                <a href="http://entree.amwhalen.com">ENTree</a> - Epsilon Nu &amp; Delta Delta Family Trees
+
+                <h3><b>Chapter Sponsor:</b></h3>
+                Thomas P. Hannum - <i></i>Associate Director, UMass Minuteman Marching Band &amp; University Concert Band<br /> <br />
+                
+                <h3>Director of Bands:</h3>
+                James Patrick Miller - <i>Director of Wind Studies, UMass Amherst<br /><br />
+                    
+                
+                <h2 style="margin: 0pt; padding: 0pt;">Spring 2013 Committees</h2><hr />
+                <i>* denotes committee chair</i><br /> <br /> 
+                
+                <table style="padding-bottom: 25px;">
+                    <tbody>
+                        <tr>
+                            <td width="195" height="322" valign="top">
+                                <p>
+                                    <b>Membership:</b><br /> 
+                                    *Dan Weaver<br />
+                                    Greg Haungs<br />
+                                    Andy Lyczmanenko<br />
+                                    Michael Bates<br />
+                                    Matt Chastain<br />
+                                    <br />
+                                    
+                                    <b>Correspondence:</b><br /> 
+                                    *Charlie Urbanowski<br />
+                                    Eddie Rouxinol<br />
+                                    Andy Lyczmanenko<br />
+                                    Michael Cortina<br />
+                                    Owen Kerrigan<br />
+                                    <br /> 
+                                    
+                                    <b>Fundraising:</b><br />
+                                    *Eddie Rouxinol<br />
+                                    Owen Kerrigan<br />
+                                    Andy Lyczmanenko<br />
+                                    Vishu Nair<br />
+                                    Andrew deMelo<br />
+                                    Mike Bates<br />
+                                    Katie Wheeler<br />
+                                    Matt Chastain<br />
+                                    Michael Cortina<br />
+                                    Dan Weaver<br />
+                                    Ethan Miller<br />
+                                    <br />
+                                </p>
+                            </td>
+                            
+                            <td width="195" valign="top">
+                                <p>
+                                    <b>Constitution:</b><br /> 
+                                    *Mike Cortina<br />
+                                    Greg Haungs<br />
+                                    Andy Lyczmanenko<br />
+                                    Charlie Urbanowski<br />
+                                    David Orleck<br />
+                                    <br /> 
+                                    
+                                    <b>Alumni Relations:</b><br>
+                                    *Dave Morris<br> 
+                                    Dan Weaver<br>
+                                    Matt Chastain<br>
+                                    Mike Bates<br>
+                                    Bolutare Doubeni<br>
+                                    <br />
+                                    
+                                    <b>Service:</b><br />
+                                    *Chris Moehringer<br />
+                                    Ricky Cabral<br />
+                                    Robert Sims<br />
+                                    Bradley Sampson<br />
+                                    David Morris<br />
+                                    Bolutare Doubeni<br />
+                                    Charlie Urbanowski<br />
+                                    Ryan Barry<br />
+                                    Adam Whitcomb<br />
+                                    Greg Haungs<br />
+                                    Joe Goguen<br />
+                                </p>
+                            </td>
+                            
+                            <td valign="top" width="195">
+                                <p>
+                                    <b>History and Restoration:</b><br /> 
+                                    *Brad Sampson<br />
+                                    Michael Bates<br />
+                                    Katie Wheeler<br />
+                                    Joe Goguen<br />
+                                    Andrew deMelo<br />
+                                    Ricky Cabral<br />
+                                    Ethan Miller<br />
+                                    <br /> 
+  
+                                    <b>PMA Correspondence:</b><br />
+                                    *Ricky Cabral<br />
+                                    Adam Whitcomb<br />
+                                    Katie Wheeler<br />
+                                    <br />
+
+                                    <b>Website:</b><br />
+                                    *Robert Sims<br />
+                                    Charlie Urbanowski<br />
+                                </p>
+                            </td>
+                            
+                            <td valign="top" width="195">
+                                <p>
+                                    <b>Scholarship:</b><br />
+                                    *Eddie Rouxinol<br />
+                                    David Morris<br />
+                                    Chris Moehringer<br />
+                                    <br />
+                                   
+                                    <b>Retreat:</b><br />
+                                    *Ryan Barry<br />
+                                    Katie Wheeler<br /> 
+                                    Michael Bates<br />
+                                </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                
+            </div>
+        </div>
+        <?php
+            include 'pages/footer.php';
+        ?>
+    </div>
+</body>
+
+</html>
--- pages/header.php
+++ pages/header.php
@@ -5,12 +5,12 @@
     <div id="navpadding">
         <ul>
             <li><b>Home</b></li>
-            <li><a href="brothers.html">Brothers</a></li>
+            <li><a href="brothers.php">Brothers</a></li>
             <li><a href="history.html">History</a></li>
             <li><a href="gallery.html">Gallery</a></li>
             <li><a href="links.html">Links</a></li>
-            <li><a href="alumni.html">Alumni</a></li>
-            <li><a href="nus.html">Newsletter</a></li>
+            <li><a href="alumni.php">Alumni</a></li>
+            <li><a href="nus.php">Newsletter</a></li>
             <li><a href="gnp.html">GNP</a></li>
         </ul>
     </div>
