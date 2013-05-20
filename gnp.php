3be21bd9aac175ef4b9c2f857183e7a7f0248e84
rsims <rsims2013@gmail.com>
2013-05-19 23:48:03 -0400

    Remade GNP page.

---
+++ gnp.php
@@ -0,0 +1,79 @@
+<?php
+    include 'pages/header.php';
+?>
+    
+<?php
+    include 'pages/navbar.php';
+?>
+
+<div id="content" style="padding-bottom:1400px">
+    <div id="content_padding">
+        <img src="images/parks.jpg" width="800" height="150" alt="Eyes With Pride"><br>
+
+        <br>
+
+        <div class="gnpcolumn" id="gnpcol-1">
+            <div class="space"></div>
+                <p>
+                    Born May 23, 1953, in Buffalo New York, George N. Parks devoted his life to inspiring generations of college and high school students to reach for the stars, to pursue music and marching band, and to become leaders and teachers in life. While growing up in Newark, Delaware, in only the ninth grade Parks decided he wanted to become a band director. Parks followed in the footsteps of his parents, Norman and Vesta Parks, who both had been drum majors, and became the drum major of the Christiana High School Viking Band. Parks later credited his band director, L. Jerome Rehberg, with having a huge impact on his life.
+                </p>
+                
+                <p>
+                    After beginning his collegiate marching career marching in the University of Delaware Fightin' Blue Hens Band, Parks transferred to West Chester University of Pennsylvania. At West Chester, he became drum major of the three-hundred-member Incomparable Golden Rams Marching Band and earned his B.A. in Music Education. "James R. Wells, director of the West Chester Band, said he admired Parks because 'on the field, he is a real ham, a showman with a definite ego-but off the field he is one of the most unassuming people you could ever meet.' He added, 'George Parks is simply the best drum major I've ever seen.' " (Kerstin Becker, <font size='1'><i>Through These Doors</i></font>)
+                </p>
+                
+                <p>
+                    After graduating from West Chester, Parks went to Northwestern to pursue his master's degree in tuba performance. There he studied under renowned tuba Professor Arnold Jacobs. He also acted as a graduate assistant under John P. Paynter and brought the first colorguard to the Big Ten.
+                </p>
+                
+                <p>
+                    In 1977, at just 24 years old, Mr. Parks was selected as the director of the University of Massachusetts Minuteman Marching Band by longtime marching band director John Jenkins. Mr. Parks' appointment was initially met with contest by the band members. The band experienced growing pains as Parks changed the band's marching style from the traditional Michigan styled "high-step" to a "roll-step". However, Parks' infectious enthusiasm and entertaining shows quickly won over the crowds and band members. Under his direction the band was transformed into the Power and Class of New England that is synonymous with UMass today. In 2005, Parks rewrote the lyrics to one of the university's fight songs, "Roll Down the Field." Parks often joked that being director of the UMMB was his first and only "real" job. In 1997, Old Chapel was closed and the band was forced to relocate its offices to the now condemned band apartments and then to the basement of Arnold House. For years, Parks campaigned tirelessly for a new permanent home for the band. His vision became a reality on October 17, 2009, when ground was broken for the George N. Parks Minuteman Marching Band Building.
+                </p>
+                
+                <p>
+                    During his 33 years teaching at the University of Massachusetts, Parks received numerous honors and accolades. For many years, he served as the honorary chairman for the UMass Amherst Charitable Campaign. In 1989, he received the University of Massachusetts Amherst Distinguished Teaching Award and in 1991 he was named the Massachusetts Instrumental Conductors Association Conductor of the Year and the Massachusetts Instrumental Conductors Association Hall of Fame. He was also awarded the Chancellor's Medal for Distinguished Service and was made an honorary alumnus in 1997. He was made an honorary Brother of the Epsilon Nu chapter of Kappa Kappa Psi and was awarded the A. Frank Martin award 1989. He was presented the Kappa Kappa Psi National Honorary Band Fraternity Distinguished Service to Music Medal, in the field of marching band, at the Multibands concert in October 2008.
+                </p>
+        </div>
+        
+        <img id="gnp-image" src='images/gnp.jpg' width='300' height='400' />
+        
+        <div class='gnpcolumn' id='gnpcol-2'>
+            <div class='space'></div>
+            <p>
+                Through the efforts of George Parks, Thom Hannum, Michael Klesch and many others, the Minuteman Marching Band received the Sudler Trophy in 1998. The trophy "is awarded annually to a college or university marching band which has demonstrated the highest musical standards and innovative marching routines and ideas, and which has made important contributions to the advancement of the performance standards of college marching bands over a period of years."
+            </p>
+            
+            <p>
+                In 1979, Parks married former clarinetist of the West Chester Golden Rams Marching Band, Jeanne, in Point Pleasant, New Jersey. Together they had two children, Kathryn and Michael. Currently, Mrs. Parks is a lead clinician of DMA and the Choral Director at Springfield Central High School, in Springfield, MA.
+            </p>
+            
+            <p>
+                Having taught marching band workshops since 1973, Parks co-founded Band Leadership Training seminars with Dr. Tim Lautzenheiser and founded the "Drum Major Academy." Each year over 3000 drum major and band leadership students attend the "nation's foremost authority on the art of drum majoring" and are educated by expert clinicians made up of active band directors, educators and professionals in the marching band industry. In 1984, Parks authored, "The Dynamic Drum Major," a guide for drum major technique and leadership.
+            </p>
+            
+            <p>
+                Along with his work at UMass Amherst and the Drum Major Academy, Parks was also the drum major for the Reading Buccaneers for 13 years. He won the DCA Championship Drum Major award eight times and led the Buccaneers to two DCA Championships in 1979 and 1980. He also worked with Bowl Games of America and conducted halftime shows at the Sugar Bowl, Orange Bowl, Gator Bowl, BCS National Championship Game, and in 2005 and 2009 was the director of the Bands of America Honor Band in the Tournament of Roses Parade. Because of his impact on the marching band world, Parks was inducted into the World Drum Corps Hall of Fame in 2006, The Bands of America Hall of Fame, and the Buccaneers Hall of Fame.
+            </p>
+            
+            <p>
+                George N. Parks passed away after a performance with the Minuteman Marching Band in Cuyahoga Falls, Ohio on the evening of September 16, 2010. The band was en route to Ann Arbor, Michigan to play during the football game between the University of Massachusetts and the University of Michigan on September 18, 2010. The remaining performances that season were dedicated in his honor, including special arrangements and drill to "I'll Be There" and "When the Saints go Marching In."
+            </p>
+            
+            <p>
+                Mr. Parks' legacy will forever live on through his starred thoughts, the energy and enthusiasm of those in the Minuteman Marching Band, the thousands that attend his Drum Major Academy each summer and the hundreds of thousands of lives he impacted.
+            </p>
+            
+            <p>
+                <div style='padding-left:120px; padding-top:50px'>
+                    <font face="palatino" size='4'><b>EYES WITH PRIDE</b></font>
+                </div>
+            </p>
+        </div>
+        
+    </div>
+
+</div>
+
+<?php
+    include 'pages/footer.php';
+?>
